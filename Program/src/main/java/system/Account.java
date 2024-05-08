6214dfdd3ac990172e0f9a694687c333ec61ec18
Ahmed Ashraf <ahmed.ashraf2827@gmail.com>
2024-05-08 18:16:16 +0300

    Updated Account class setters and getters

--- Program/src/main/java/system/Account.java
+++ Program/src/main/java/system/Account.java
@@ -19,7 +19,7 @@ public abstract class Account
       this.credit = credit;
       this.age = age;
    }
-
+   
    public String getUserName()
    {
       return this.userName;
@@ -29,6 +29,27 @@ public abstract class Account
    {
       return this.password;
    }
+
+   public String getFullName()
+   {
+      return fullName;
+   }
+
+   public String getEmail()
+   {
+      return email;
+   }
+
+   public double getCredit()
+   {
+      return credit;
+   }
+
+   public int getAge()
+   {
+      return age;
+   }
+   
    
    public void setUserName(String userName)
    {
@@ -39,6 +60,26 @@ public abstract class Account
    public void setEmail(String email)
    {
       this.email = email;
+   }
+
+   public void setFullName(String fullName)
+   {
+      this.fullName = fullName;
+   }
+
+   public void setPassword(String password)
+   {
+      this.password = password;
+   }
+
+   public void setAge(int age)
+   {
+      this.age = age;
+   }
+
+   public void setCredit(double credit)
+   {
+      this.credit = credit;
    }
    
    abstract public void displayInfo();
