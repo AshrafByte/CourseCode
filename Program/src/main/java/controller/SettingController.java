5fbda8da6b498bfb4282858049981ead95d15cbb
Ahmed Ashraf <ahmed.ashraf2827@gmail.com>
2024-05-08 18:15:27 +0300

    Updated Settting page controller

--- Program/src/main/java/controller/SettingController.java
+++ Program/src/main/java/controller/SettingController.java
@@ -7,8 +7,13 @@ package controller;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -18,30 +23,69 @@ import javafx.fxml.Initializable;
 public class SettingController implements Initializable
 {
 
+   @FXML
+   TextField name ;
+   @FXML
+   TextField password ;
+   @FXML
+   TextField age ; 
+   @FXML
+   TextField email ;
+   @FXML
+   Label savedMesg ;
+
    /**
     * Initializes the controller class.
     */
    @Override
    public void initialize(URL url, ResourceBundle rb)
    {
-      // TODO
-   }   
-   
+      var account = App.account;
+      name.setPromptText(account.getFullName());
+      password.setPromptText(account.getPassword());
+      email.setPromptText(account.getEmail());
+      age.setPromptText(String.valueOf(account.getAge()));
+   }
+
    @FXML
    public void goToDashboard() throws IOException
    {
       App.setRoot("dashboard");
    }
-   
+
    @FXML
    public void goToLogin() throws IOException
    {
       App.setRoot("login");
    }
-   
+
    @FXML
    public void goToHome() throws IOException
    {
       App.setRoot("home");
+   }
+   
+   private void playSavedAnimation()
+   {
+      FadeTransition ft1 = new FadeTransition(Duration.seconds(5),savedMesg);
+      savedMesg.setVisible(true);
+      ft1.setFromValue(1.0);
+      ft1.setToValue(0);
+      ft1.play();   
+   }
+   @FXML
+   public void saveChangesAction()
+   {  
+      if (!name.getText().equals(""))
+         App.account.setUserName(name.getText());
+      if (!email.getText().equals(""))
+         App.account.setEmail(email.getText());
+      if (!password.getText().equals(""))
+         App.account.setPassword(password.getText());
+      if (!age.getText().equals(""))
+         App.account.setAge(Integer.parseInt(age.getText()));
+      if (!name.getText().equals("") || !email.getText().equals("") || !password.getText().equals("") || !age.getText().equals(""))
+         playSavedAnimation();
+      
    }
 }
