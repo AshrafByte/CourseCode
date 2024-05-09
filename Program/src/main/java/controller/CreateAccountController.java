75df9921c49207b311cbdc0bab644f44d0958882
Ahmed Ashraf <ahmed.ashraf2827@gmail.com>
2024-05-10 02:08:20 +0300

    Adding create account feature

--- Program/src/main/java/controller/CreateAccountController.java
+++ Program/src/main/java/controller/CreateAccountController.java
@@ -4,18 +4,38 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import system.Account;
+import system.Instructor;
+import system.LMS;
+import system.Student;
 
 /**
  * FXML Controller class
- *
- * @author A.Ashraf
- */
+**/
 public class CreateAccountController implements Initializable
 {
-
+   @FXML
+   TextField username;
+   @FXML
+   TextField email;
+   @FXML
+   PasswordField password;
+   @FXML
+   PasswordField confirmPassword;
+   @FXML
+   Label errorL ;
+   @FXML
+   RadioButton accountType;
    /**
     * Initializes the controller class.
     */
@@ -25,4 +45,40 @@ public class CreateAccountController implements Initializable
       // TODO
    }   
    
+ 
+   private boolean isValidFields()
+   {
+      if (password.getText().equals("") || username.getText().equals("") || email.getText().equals("") || confirmPassword.getText().equals(""))
+      {
+         errorL.setText("Please provide all data fields");
+         return false;
+      }
+      if (LMS.findAccount(username.getText()) != null)
+      {
+         errorL.setText("This username already existed");
+         return false;
+      }
+      if (!(password.getText().equals(confirmPassword.getText())))
+      {
+         errorL.setText("Password doesn't match");
+         return false;
+      }
+      return true;
+   }
+   @FXML
+   private void register() throws IOException
+   {
+      if (isValidFields())
+      {
+         if (accountType.isSelected())
+            App.account = new Instructor(username.getText(), password.getText(), email.getText());
+         else
+            App.account = new Student(username.getText(), password.getText(), email.getText());
+         LMS.createAccount(App.account);
+         App.setRoot("home");
+      }
+      else
+         App.playErrorAnimation(errorL);
+         
+   }
 }
